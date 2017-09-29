573345159fd6adf4be0088c49fb6f02fb7ea899b
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-29 11:29:53 -0500

    change next key strategy to wait a bit

--- lib/network/keys.ex
+++ lib/network/keys.ex
@@ -2,15 +2,34 @@ defmodule Nb.Keys do
   use Agent
 
   @keys Application.get_env(:nb, :keys)
+  @key_interval 100
+
 
   # State takes the format index
   def start_link do
-    Agent.start_link(fn -> 0 end, name: __MODULE__)
+    Agent.start_link(fn ->
+      Enum.map(@keys, fn
+        key -> {key, Timex.now()}
+      end)
+    end, name: __MODULE__)
   end
 
+  # Return first key task's awaiting
   def next_key do
-    idx = Agent.get __MODULE__, fn idx -> idx end
-    Agent.update __MODULE__, fn idx -> rem(idx + 1, length(@keys)) end
-    @keys |> Enum.at(idx)
+    Agent.get_and_update __MODULE__, fn [this | rest] ->
+      {key, ready_at} = this
+
+      if Timex.before?(Timex.now(), ready_at) do
+        Timex.now()
+        |> Timex.diff(ready_at, :milliseconds)
+        |> abs()
+        |> :timer.sleep()
+      end
+
+      next_ready_at = Timex.shift(Timex.now(), milliseconds: @key_interval)
+      next_state = Enum.concat(rest, [{key, next_ready_at}])
+
+      {key, next_state}
+    end
   end
 end
