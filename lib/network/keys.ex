e8d2aac3bdbdf990405e4f08035bae176888f9d4
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-09-29 14:03:18 -0500

    properly update before sleeping

--- lib/network/keys.ex
+++ lib/network/keys.ex
@@ -2,7 +2,7 @@ defmodule Nb.Keys do
   use Agent
 
   @keys Application.get_env(:nb, :keys)
-  @key_interval 1000
+  @key_interval 200
 
 
   # State takes the format index
@@ -16,20 +16,24 @@ defmodule Nb.Keys do
 
   # Return first key task's awaiting
   def next_key do
-    Agent.get_and_update __MODULE__, fn [this | rest] ->
+    {key, ready_at} = Agent.get_and_update __MODULE__, fn [this | rest] ->
       {key, ready_at} = this
-
-      if Timex.before?(Timex.now(), ready_at) do
-        Timex.now()
-        |> Timex.diff(ready_at, :milliseconds)
-        |> abs()
-        |> :timer.sleep()
-      end
 
       next_ready_at = Timex.shift(Timex.now(), milliseconds: @key_interval)
       next_state = Enum.concat(rest, [{key, next_ready_at}])
 
-      {key, next_state}
+      {{key, ready_at}, next_state}
     end
+
+    if Timex.before?(Timex.now(), ready_at) do
+      Timex.now()
+      |> Timex.diff(ready_at, :milliseconds)
+      |> abs()
+      |> :timer.sleep()
+
+      IO.puts "sleeping for #{key}"
+    end
+
+    key
   end
 end
